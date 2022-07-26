d4974e49b353db373cd0093e1df1523e8b17d7cc
0xlucyfer <mrfancypants123321@gmail.com>
2022-07-26 13:15:19 -0700

    working translator. Needs tests

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 - Clone
 - Run `python3 -m venv venv`.
 - Run `. venv/bin/activate`.
+- Run `npm i`
 - Run `python install -e .`.
 - Run `run` to execute script.
--- scripts/translator.py
+++ scripts/translator.py
@@ -1,6 +1,58 @@
+import glob
+import os
+import json
+import pandas as pd
+
+from googletrans import Translator, constants
+from pprint import pprint
+from typing import List
+
+
+
 def translate():
-    from pprint import pprint
-    print('Entry is working!')
-    # import pdb; pdb.set_trace()
-    print('But what if I make a change??')
-    # main()
+    translator = Translator()
+    csv_files = get_csv_file_paths()
+
+    for file in csv_files:
+        # Read csv file
+        df = pd.read_csv(file)
+        # Get df dimensions - (col, row)
+        shape = df.shape
+        count = 0
+        spanish_translations = []
+
+        # Iterate over all columns
+        # shape[0] should equal count
+        for index in range(shape[0]):
+            eng_word = df.values[index, 0]
+            translation = translator.translate(eng_word, src='en', dest='es')
+            spanish_translations.append(translation.text)
+            count+=1
+            print(f"EN: {eng_word} || ES: {translation.text}")
+            # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+        break
+    import pdb; pdb.set_trace()
+
+
+def get_csv_file_paths(csv_files: str = '/collections') -> List[str]:
+    '''
+        Load CSV files.
+        csv_files (str): Location of csv files
+    '''
+    path = os.getcwd()
+    path = path + csv_files
+    return glob.glob(os.path.join(path, "*.csv"))
+
+def load_fixture(file_path='/tests/fixtures', fix_name=None):
+    '''
+        Inject fixtures so we dont need to 
+        run calculations each time.
+    '''
+    # Set fixture vars
+    FIX_BASE = path = os.getcwd()
+    SWAGGER_FIX = 'example_ingestion_manager_swagger.json'
+
+    fixture_path = f'{ os.getcwd()}{file_path}'
+    with open(fixture_path) as json_file:
+        fixture = json.load(json_file)
+    return fixture
