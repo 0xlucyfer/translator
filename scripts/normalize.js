559c17de2edc711562a140de14118d32ade2f315
0xlucyfer <mrfancypants123321@gmail.com>
2022-07-27 10:03:49 -0700

    removed testing code from scripts

--- scripts/normalize.js
+++ scripts/normalize.js
@@ -1,22 +1,23 @@
 const fs = require('fs'); 
 var namehash = require('@ensdomains/eth-ens-namehash');
 
-var hash = namehash.hash('cártel')
-console.log(hash)
-// try {  
-//     // var es_nouns = fs.readFileSync('tests/fixtures/es-nouns.txt').toString().split("\n");
-//     var es_verbs = fs.readFileSync('tests/fixtures/es-verbs.txt').toString().split("\n");
-//     console.log(es_verbs);
-// } catch(e) {
-//     console.log('Error:', e.stack);
-// }
 
-// // es_nouns.forEach(hash);
-// es_verbs.forEach(hash);
+try {  
+    var es_nouns = fs.readFileSync('tests/fixtures/es-nouns.txt').toString().split("\n");
+    var es_verbs = fs.readFileSync('tests/fixtures/es-verbs.txt').toString().split("\n");
+    // console.log(es_nouns);
+    // console.log(es_verbs);
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
-// function hash(value) {
-//     var hash = namehash.hash(value)
-//     hash = hash.replace('0x', '');
-//     console.log(value + "," + hash)
-// }
+es_nouns.forEach(hash);
+console.log('\n\n\n\n');
+es_verbs.forEach(hash);
+
+function hash(value) {
+    var hash = namehash.hash(value)
+    hash = hash.replace('0x', '');
+    console.log(value + "," + hash)
+}
 
--- scripts/translator.py
+++ scripts/translator.py
@@ -14,34 +14,31 @@ def translate():
         Loads, reads, translates, QAs, & prints english words to spanish.
         Saves new csv file for each file in collections.
     '''
-    translator = Translator()
-    translation = translator.translate('dragon', src='en', dest='es')
-    print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
-    # csv_files = get_csv_file_paths()
-    # total_translated_lists = {}
+    csv_files = get_csv_file_paths()
+    total_translated_lists = {}
 
-    # for file in csv_files:
-    #     f = open(f"tests/fixtures/{file.split('/')[-1]}.txt", 'w')
-    #     print(f"Working on translating {file.split('/')[-1]} into spanish...")
+    for file in csv_files:
+        f = open(f"tests/fixtures/{file.split('/')[-1]}.txt", 'w')
+        print(f"Working on translating {file.split('/')[-1]} into spanish...")
         
-    #     # Read csv file
-    #     df = pd.read_csv(file)
-    #     shape = df.shape
-    #     spanish_translations = []
+        # Read csv file
+        df = pd.read_csv(file)
+        shape = df.shape
+        spanish_translations = []
 
-    #     # Iterate down column 0, translate + clean word.
-    #     for index in range(shape[0]):
-    #         eng_word = df.values[index, 0]
-    #         translation = translator.translate(eng_word, src='en', dest='es')
-    #         spanish_translations.append((((translation.text).lower()).strip()).replace(" ", ""))
-    #         # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+        # Iterate down column 0, translate + clean word.
+        for index in range(shape[0]):
+            eng_word = df.values[index, 0]
+            translation = translator.translate(eng_word, src='en', dest='es')
+            spanish_translations.append((((translation.text).lower()).strip()).replace(" ", ""))
+            # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
 
-    #     # Remove dups
-    #     spanish_translations = list(dict.fromkeys(spanish_translations))
-    #     for word in spanish_translations:
-    #         f.write(word)
-    #         f.writelines('\n')
-    #     f.close()
+        # Remove dups
+        spanish_translations = list(dict.fromkeys(spanish_translations))
+        for word in spanish_translations:
+            f.write(word)
+            f.writelines('\n')
+        f.close()
 
 
 def get_csv_file_paths(csv_files: str = '/collections') -> List[str]:
