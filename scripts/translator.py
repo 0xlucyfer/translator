83370b8bd8dcb5522c1b87f7e606dc01d363fb46
0xlucyfer <mrfancypants123321@gmail.com>
2022-07-27 10:01:02 -0700

    have all nouns & verbs. Manual work required but good enough. Working on scraping english animals

--- scripts/translator.py
+++ scripts/translator.py
@@ -3,41 +3,45 @@ import os
 import json
 import pandas as pd
 
-from googletrans import Translator, constants
+from googletrans import Translator
 from pprint import pprint
 from typing import List, Dict
 
 
-
-def translate() -> Dict[str, List[str]]:
+# import pdb; pdb.set_trace()
+def translate():
+    '''
+        Loads, reads, translates, QAs, & prints english words to spanish.
+        Saves new csv file for each file in collections.
+    '''
     translator = Translator()
-    csv_files = get_csv_file_paths()
-    total_translated_lists = {}
+    translation = translator.translate('dragon', src='en', dest='es')
+    print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+    # csv_files = get_csv_file_paths()
+    # total_translated_lists = {}
 
-    for file in csv_files:
-        print(f"Working on translating {file.split('/')[-1]} into spanish...")
+    # for file in csv_files:
+    #     f = open(f"tests/fixtures/{file.split('/')[-1]}.txt", 'w')
+    #     print(f"Working on translating {file.split('/')[-1]} into spanish...")
         
-        # Read csv file
-        df = pd.read_csv(file)
-        # Get df dimensions - (col, row)
-        shape = df.shape
-        count = 0
-        spanish_translations = []
+    #     # Read csv file
+    #     df = pd.read_csv(file)
+    #     shape = df.shape
+    #     spanish_translations = []
 
-        # Iterate over all columns
-        # shape[0] should equal count
-        for index in range(shape[0]):
-            eng_word = df.values[index, 0]
-            translation = translator.translate(eng_word, src='en', dest='es')
-            spanish_translations.append((((translation.text).lower()).strip()).replace(" ", ""))
-            count+=1
-            # print(f"EN: {eng_word} || ES: {translation.text}")
-            # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
-        # import pdb; pdb.set_trace()
-        print(f"Translated {count} english words into spanish from {file.split('/')[-1]}...")
-        total_translated_lists[(file.split('/'))[-1]] = spanish_translations
-    # import pdb; pdb.set_trace()
-    return total_translated_lists
+    #     # Iterate down column 0, translate + clean word.
+    #     for index in range(shape[0]):
+    #         eng_word = df.values[index, 0]
+    #         translation = translator.translate(eng_word, src='en', dest='es')
+    #         spanish_translations.append((((translation.text).lower()).strip()).replace(" ", ""))
+    #         # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+
+    #     # Remove dups
+    #     spanish_translations = list(dict.fromkeys(spanish_translations))
+    #     for word in spanish_translations:
+    #         f.write(word)
+    #         f.writelines('\n')
+    #     f.close()
 
 
 def get_csv_file_paths(csv_files: str = '/collections') -> List[str]:
