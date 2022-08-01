d279f3813b305b8f0101ec3431cf04eb1e2bf91b
0xlucyfer <mrfancypants123321@gmail.com>
2022-08-01 14:17:18 -0700

    finally able to mock n numbers of opens

--- tests/unit/test_translator.py
+++ tests/unit/test_translator.py
@@ -28,22 +28,17 @@ mock_file_data = {
 
 class TestTranslator(TestCase):
 
-    # @patch("scripts.translator.open")
+    @patch('scripts.translator.open', new_callable=mock_open, read_data="aa\nbb")
     # @patch("scripts.translator.open", mock_open(read_data='test')) # Mocks Translator().
     @patch('scripts.translator.Translator', return_value=FakeTranslationObj) # Mocks Translator().
-    def test_basic(self, mock_translation):
-        # mo.return_value.__enter__.return_value = mock_open(read_data='test')
+    def test_basic(self, mock_translation, mo):
 
         # Mocks cmd to run script
         with mock.patch('sys.argv', ['run-txt', 'nouns/spanish-nouns.txt', 'file2.txt']):
             # with patch("builtins.open", mock_open(read_data="data")) as mock_file:
-            with patch('scripts.translator.open', create=True) as mo:
-                # mo.return_value = MagicMock()
-                reads = ['text1', 'text2']
-                mo.return_value.__enter__.return_value.read.side_effect = lambda: reads.pop(0)
-        
-                # m_file.read.side_effect = lambda: reads.pop(0)
-                translate_txt()
+            handlers = [mo.return_value, mock_open(read_data="AA\nBB").return_value, mock_open(read_data="666\n666").return_value]
+            mo.side_effect = handlers
+            translate_txt()
 
     # @patch('scripts.translator.Translator', return_value=[666])
     # def test_command(self, mock_item):
