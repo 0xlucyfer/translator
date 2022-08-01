4152530c1d0acb57ff3bd406b2fdbe6c4e6bf2a5
0xlucyfer <mrfancypants123321@gmail.com>
2022-08-01 15:39:54 -0700

    have a working test

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+import os
+import pytest
+from unittest.mock import patch, mock_open
+from unittest import TestCase, mock
--- tests/unit/test_translator.py
+++ tests/unit/test_translator.py
@@ -9,36 +9,31 @@ from scripts.translator import (
     translate_csv,
     translate_txt
 )
-from scripts.utils import multi_mock_open, FIXTURE_PATH
+from scripts.utils import FIXTURE_PATH
+from unittest.mock import call
 
-# from mock_open import MockOpen
-mock_file_data = {
-    "file1.txt": "some text 1",
-    "file2.txt": "some text 2",
-    # ... and so on ...
-}
-
-
-# @pytest.fixture()
-# def mocked_open(mocker):
-#     m = mocker.patch("builtins.open")
-#     m.side_effect = partial(mocked_file, m)
-#     m.assert_opened = partial(assert_opened, m)
-#     return m
-
+# import pdb; pdb.set_trace()
 class TestTranslator(TestCase):
-
+    @patch('scripts.translator.create_ordered_alpha_txt_files', return_value={})
     @patch('scripts.translator.open', new_callable=mock_open, read_data="aa\nbb")
-    # @patch("scripts.translator.open", mock_open(read_data='test')) # Mocks Translator().
     @patch('scripts.translator.Translator', return_value=FakeTranslationObj) # Mocks Translator().
-    def test_basic(self, mock_translation, mo):
+    def test_translate_txt_files(self, mock_translation, mo, mock_COATF):
+        '''
+            Test file reading & writing in translate_txt.
+        '''
+        # Setup
+        expected = [call('tests/fixtures/nouns/spanish-nouns.txt', 'r'), call('tests/fixtures/file2.txt', 'w')]
 
-        # Mocks cmd to run script
         with mock.patch('sys.argv', ['run-txt', 'nouns/spanish-nouns.txt', 'file2.txt']):
-            # with patch("builtins.open", mock_open(read_data="data")) as mock_file:
+            # Set with open return values.
             handlers = [mo.return_value, mock_open(read_data="AA\nBB").return_value, mock_open(read_data="666\n666").return_value]
             mo.side_effect = handlers
+            # Function under test
             translate_txt()
+
+        actual = mo.call_args_list
+        assert actual == expected
+        assert mo.call_count == 2
 
     # @patch('scripts.translator.Translator', return_value=[666])
     # def test_command(self, mock_item):
